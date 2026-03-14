2843e3077c11d1e4e155d3420f184b36d63d95d5
Pcromeo <157694396+Pcromeo@users.noreply.github.com>
2026-03-14 16:23:06 +0100

    Update datetime.py

--- custom_components/evodnik/datetime.py
+++ custom_components/evodnik/datetime.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 from datetime import timedelta
+from typing import Any
 
 from homeassistant.components.datetime import DateTimeEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt as dt_util
+
+from .const import DOMAIN, CONF_DEVICE_NAME, CONF_DEVICE_ID
 
 
 async def async_setup_entry(
@@ -14,22 +18,53 @@ async def async_setup_entry(
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    data = coordinator.data or {}
+
+    headers = (data or {}).get("headers", [])
+    hdr0 = headers[0] if headers else {}
+    device_number = hdr0.get("DeviceNumber")
+    device_name = entry.data.get(CONF_DEVICE_NAME) or hdr0.get("DeviceName") or f"Device {entry.data.get(CONF_DEVICE_ID)}"
+
     now = dt_util.now().replace(second=0, microsecond=0)
 
     async_add_entities(
         [
-            EvodnikVacationFrom(entry, now),
-            EvodnikVacationTo(entry, now + timedelta(days=7)),
+            EvodnikVacationFrom(entry, device_number, device_name, hdr0, now),
+            EvodnikVacationTo(entry, device_number, device_name, hdr0, now + timedelta(days=7)),
         ]
     )
 
 
 class EvodnikBaseDateTime(DateTimeEntity):
-    def __init__(self, entry: ConfigEntry, name: str, unique_suffix: str, initial_value) -> None:
+    _attr_entity_category = EntityCategory.CONFIG
+
+    def __init__(
+        self,
+        entry: ConfigEntry,
+        device_number: Any,
+        device_name: str,
+        hdr: dict,
+        name: str,
+        unique_suffix: str,
+        initial_value,
+    ) -> None:
         self._entry = entry
+        self._device_number = device_number
+        self._device_name = device_name
+        self._hdr = hdr
         self._attr_name = name
         self._attr_native_value = initial_value
         self._attr_unique_id = f"{entry.entry_id}_{unique_suffix}"
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, f"{self._device_number}")},
+            "manufacturer": "eVodník",
+            "name": f"eVodník {self._device_name}",
+            "model": f'{self._hdr.get("Version","")}/{self._hdr.get("VersionNumber","")}',
+        }
 
     async def async_set_value(self, value) -> None:
         self._attr_native_value = value
@@ -37,9 +72,12 @@ class EvodnikBaseDateTime(DateTimeEntity):
 
 
 class EvodnikVacationFrom(EvodnikBaseDateTime):
-    def __init__(self, entry: ConfigEntry, initial_value) -> None:
+    def __init__(self, entry, device_number, device_name, hdr, initial_value) -> None:
         super().__init__(
             entry=entry,
+            device_number=device_number,
+            device_name=device_name,
+            hdr=hdr,
             name="Dovolená - od",
             unique_suffix="vacation_from",
             initial_value=initial_value,
@@ -47,9 +85,12 @@ class EvodnikVacationFrom(EvodnikBaseDateTime):
 
 
 class EvodnikVacationTo(EvodnikBaseDateTime):
-    def __init__(self, entry: ConfigEntry, initial_value) -> None:
+    def __init__(self, entry, device_number, device_name, hdr, initial_value) -> None:
         super().__init__(
             entry=entry,
+            device_number=device_number,
+            device_name=device_name,
+            hdr=hdr,
             name="Dovolená - do",
             unique_suffix="vacation_to",
             initial_value=initial_value,
